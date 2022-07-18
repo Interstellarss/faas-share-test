f3bed5bd60433c3bf3b6eecb2c3217c4bdf3c5a5
Puxuan <leslie.qq.lol@gmail.com>
2022-07-18 12:56:33 +0200

    new dockerfile and py for init

--- mobbilenet2/mobilenet.py
+++ mobbilenet2/mobilenet.py
@@ -182,7 +182,7 @@ def mobilenet_v2(pretrained=False, progress=True, **kwargs):
     """
     model = MobileNetV2(**kwargs)
     if pretrained:
-        state_dict = torch.load("mobilenet_v2-b0353104.pth", map_location=torch.device("cuda"))
+        state_dict = torch.load("/model/mobilenet/mobilenet_v2-b0353104.pth", map_location=torch.device("cuda"))
         model.load_state_dict(state_dict)
     return model
 
