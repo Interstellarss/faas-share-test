b086610dfa8acece88c4da98fa2029fab3cac1f3
Yichao Zhu <yukio.zhu@tum.de>
2022-09-07 18:07:55 +0200

    add paddlespeech readinessprobe

--- paddlespeech/test.sh
+++
@@ -1,5 +0,0 @@
-wget -c https://paddlespeech.bj.bcebos.com/PaddleAudio/zh.wav
-{   printf '{"audio": "'
-    base64 zh.wav
-    printf '", "audio_format":"wav", "sample_rate": 16000, "lang": "zh_cn"}'
-}|  curl -H "Content-Type: application/json" --data @- -X POST localhost:8080/paddlespeech/asr
